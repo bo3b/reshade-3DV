bd1cfdbc85711c0013816ebacc4a3c73d34f627d
bo3b <bitbucket@bo3b.net>
2021-03-02 12:45:55 -0800

    Add D3D11_CREATE_DEVICE_DEBUG on Debug builds.
    Not sure why this isn't default, but I never want to run a debug build without the debug layer active.

--- source/d3d11/d3d11.cpp
+++ source/d3d11/d3d11.cpp
@@ -55,6 +55,7 @@ HOOK_EXPORT HRESULT WINAPI D3D11CreateDeviceAndSwapChain(IDXGIAdapter *pAdapter,
 #ifndef NDEBUG
 	// Remove flag that prevents turning on the debug layer
 	Flags &= ~D3D11_CREATE_DEVICE_PREVENT_ALTERING_LAYER_SETTINGS_FROM_REGISTRY;
+	Flags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
 	// Use local feature level variable in case the application did not pass one in
