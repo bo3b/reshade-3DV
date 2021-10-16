1692d1bad06c944f3b7cdec1fb8b3f3bfebc3f02
bo3b <bitbucket@bo3b.net>
2021-10-16 05:59:03 -0700

    Experimental changes for sharing surfaces in DX12. Mostly fail.
    Didn't seem to work, get an error at the DX11 usage of 'invalid argument'.  No reason why, no hint from debug layer.
    
    This code might be helpful, so saving it as an exp branch.

--- source/d3d12/vr_d3d12.cpp
+++ source/d3d12/vr_d3d12.cpp
@@ -19,6 +19,8 @@
 #include "dll_log.hpp"
 #include "dxgi/format_utils.hpp"
 
+#include "d3d12.h"
+#include "d3dx12.h""
 
 vr_d3d12::vr_d3d12(IDXGISwapChain3 *swapchain)
 {
@@ -29,6 +31,122 @@ vr_d3d12::~vr_d3d12()
 {
 	ReleaseFileMappedIPC();
 }
+
+// ----------------------------------------------------------------------
+
+// Get the shared surface specified by the input HANDLE.  This will be from 
+// the game side, in DX9Ex. This technique is specified in the documentation:
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ff476531(v=vs.85).aspx
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ee913554(v=vs.85).aspx
+
+// For the shared surface itself, disposed when recreated.
+ID3D11Texture2D* pTexture2D = nullptr;
+ID3D11ShaderResourceView* pSRView = nullptr;
+
+HANDLE CreateDX11SharedSurface(ID3D11Device* pDevice, IUnknown* gameDoubleTex)
+{
+	HRESULT hr;
+	D3D11_TEXTURE2D_DESC desc = { 0 };
+	//IUnknown* oldGameTexture = _shared_texture;
+	//ID3D11Device* pDevice = nullptr;
+
+	//reinterpret_cast<ID3D11Texture2D*>(gameDoubleTex)->GetDesc(&desc);
+	//reinterpret_cast<ID3D11Texture2D*>(gameDoubleTex)->GetDevice(&pDevice);
+
+	//LOG(INFO) << "vr::CreateSharedTexture called. _shared_texture: " << _shared_texture << " _game_sharedhandle: " << _game_sharedhandle << " _mapped_view: " << _mapped_view;
+
+	//LOG(INFO) << "  | DoubleTex                               |                                         |";
+	//LOG(INFO) << "  +-----------------------------------------+-----------------------------------------+";
+	//LOG(INFO) << "  | Width                                   | " << std::setw(39) << desc.Width << " |";
+	//LOG(INFO) << "  | Height                                  | " << std::setw(39) << desc.Height << " |";
+	//if (const char *format_string = format_to_string(desc.Format); format_string != nullptr)
+	//	LOG(INFO) << "  | Format                                  | " << std::setw(39) << format_string << " |";
+	//else
+	//	LOG(INFO) << "  | Format                                  | " << std::setw(39) << desc.Format << " |";
+	//LOG(INFO) << "  +-----------------------------------------+-----------------------------------------+";
+
+
+
+	// This texture needs to use the Shared flag, so that we can share it to 
+// another Device.  Because these are all DX11 objects, the share will work.
+
+	desc.BindFlags |= D3D11_BIND_SHADER_RESOURCE;	// Must add bind flag, so SRV can be created in Unity.
+	desc.MiscFlags = D3D11_RESOURCE_MISC_SHARED;	// To be shared. maybe D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX is better
+													// But we never seem to see any contention between game and Katanga.
+
+	hr = pDevice->CreateTexture2D(&desc, NULL, reinterpret_cast<ID3D11Texture2D**>(&_shared_texture));
+	if (FAILED(hr)) FatalExit(L"Fail to create shared stereo Texture", hr);
+
+	// Now create the HANDLE which is used to share surfaces.  This follows the model from:
+	// https://docs.microsoft.com/en-us/windows/desktop/api/d3d11/nf-d3d11-id3d11device-opensharedresource
+
+	IDXGIResource* pDXGIResource = NULL;
+	hr = _shared_texture->QueryInterface(__uuidof(IDXGIResource), (LPVOID*)&pDXGIResource);
+	{
+		if (FAILED(hr))	FatalExit(L"Fail to QueryInterface on shared surface", hr);
+
+		hr = pDXGIResource->GetSharedHandle(&_game_sharedhandle);
+		if (FAILED(hr) || _game_sharedhandle == NULL) FatalExit(L"Fail to pDXGIResource->GetSharedHandle", hr);
+	}
+	pDXGIResource->Release();
+
+	LOG(INFO) << "  Successfully created new DX11 _shared_texture: " << _shared_texture << ", new shared _game_sharedhandle: " << _game_sharedhandle;
+
+	// Move that shared handle into the MappedView to IPC the Handle to Katanga.
+	// The HANDLE is always 32 bit, even for 64 bit processes.
+	// https://docs.microsoft.com/en-us/windows/win32/winprog64/interprocess-communication
+	//
+	// This magic line of code will fire off the Katanga side rebuilding of it's drawing pipeline and surface.
+
+	*(PUINT)(_mapped_view) = PtrToUint(_game_sharedhandle);
+
+
+	//HRESULT hr;
+
+	//// Even though the input shared surface is a RenderTarget Surface, this
+	//// Query for Texture2D still works.  Not sure if it is good or bad.
+	//ID3D11Device1* pDevice1 = nullptr;
+	//hr = pDevice->QueryInterface(__uuidof(ID3D11Device1), (void**)&pDevice1);
+	//{
+	//	if (FAILED(hr) || (pDevice1 == nullptr)) DebugBreak(); // FatalExit(L"Failed to upcast ID3D11Device->ID3D11Device1.", hr);
+
+	//	hr = pDevice1->OpenSharedResource1(shared, __uuidof(ID3D11Texture2D), (void**)(&pTexture2D));
+	//	LOG(INFO) << "....OpenSharedResource on shared: " << shared  << ", result: " << hr << ", resource: " << pTexture2D;
+
+	//	if (FAILED(hr) || (pTexture2D == nullptr)) DebugBreak(); // FatalExit(L"Failed to open shared surface.", hr);
+	//}
+	//pDevice1->Release();
+
+	// By capturing the Width/Height/Format here, we can let Unity side
+	// know what buffer to build to match.
+	//D3D11_TEXTURE2D_DESC tdesc;
+	//UINT gWidth;
+	//UINT gHeight;
+	//DXGI_FORMAT gFormat;
+	//pTexture2D->GetDesc(&tdesc);
+	//gWidth = tdesc.Width;
+	//gHeight = tdesc.Height;
+	//gFormat = tdesc.Format;
+
+	//LOG(INFO) << "....Successful GetDesc on surface - Width: %d, Height: %d, Format: %d\n" << 
+	//	tdesc.Width << tdesc.Height << tdesc.Format;
+
+	//// This is theoretically the exact same surface in the video card memory,
+	//// that the game's DX11 is using as the stereo shared surface. 
+	////
+	//// Now we need to create a ShaderResourceView using this, because that
+	//// is what Unity requires for its CreateExternalTexture.
+	////
+	//// No need to change description, we want it to be the same as what the game
+	//// specifies, so passing NULL to make it identical.
+
+	//hr = pDevice->CreateShaderResourceView(pTexture2D, NULL, &pSRView);
+	//LOG(INFO) << "....CreateShaderResourceView on texture: %p, result: %d, SRView: %p\n" <<  pTexture2D <<  hr << pSRView;
+	//if (FAILED(hr))	DebugBreak(); // FatalExit(L"Failed to CreateShaderResourceView.", hr);
+
+	//return pSRView;
+}
+
 
 // ----------------------------------------------------------------------
 // For when we need to create the offscreen Texture2D for our stereo
@@ -93,47 +211,107 @@ void vr_d3d12::CreateSharedTexture(IUnknown* gameDoubleTex)
 	LOG(INFO) << "  | Final Format                            | " << std::setw(39) << format_to_string(res_desc.Format) << " |";
 
 
+
+	// Make a dx11 texture that works, as the surface to be shared, and then fetch reference
+	// to it here as DX12
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	ID3D11Device* g_pd3dDevice;
+	hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, &featureLevels[1], numFeatureLevels - 1,
+		D3D11_SDK_VERSION, &g_pd3dDevice, nullptr, nullptr);
+	if (FAILED(hr)) FatalExit(L"Fail to create text DX11", hr);
+	LOG(INFO) << "  Successfully create test dx11 device: " << g_pd3dDevice;
+
+	_game_sharedhandle = CreateDX11SharedSurface(g_pd3dDevice, gameDoubleTex);
+
+
+
 	// Create destination texture for sharing. Same Desc as the input DoubleTex.
 	// This will be a 2x buffer.
 	// https://stackoverflow.com/questions/52869111/sharing-id3d11buffer-and-id3d12resource
 
-	ID3D12Resource* share;
-	D3D12_HEAP_PROPERTIES heap_props = { D3D12_HEAP_TYPE_DEFAULT };
-	D3D12_HEAP_FLAGS heap_flags = D3D12_HEAP_FLAG_SHARED;
-	res_desc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET | D3D12_RESOURCE_FLAG_ALLOW_SIMULTANEOUS_ACCESS;
-	D3D12_RESOURCE_STATES res_states = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
-	D3D12_CLEAR_VALUE* clear_value = nullptr;
+//	ID3D12Resource* share;
+//	D3D12_HEAP_PROPERTIES heap_props = { D3D12_HEAP_TYPE_DEFAULT };
+//	D3D12_HEAP_FLAGS heap_flags = D3D12_HEAP_FLAG_SHARED;
+//	res_desc.Flags |= D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET | D3D12_RESOURCE_FLAG_ALLOW_SIMULTANEOUS_ACCESS;
+//	D3D12_RESOURCE_STATES res_states = D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+//	D3D12_CLEAR_VALUE* clear_value = nullptr;
+//
+//
+////	Microsoft::WRL::ComPtr<ID3D12Resource>      m_videoTexture;
+//	CD3DX12_RESOURCE_DESC desc(
+//		D3D12_RESOURCE_DIMENSION_TEXTURE2D,
+//		0,
+//		res_desc.Width,
+//		res_desc.Height,
+//		1,
+//		1,
+//		DXGI_FORMAT_B8G8R8A8_UNORM,
+//		1,
+//		0,
+//		D3D12_TEXTURE_LAYOUT_UNKNOWN,
+//		D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET | D3D12_RESOURCE_FLAG_ALLOW_SIMULTANEOUS_ACCESS);
+//
+//	CD3DX12_HEAP_PROPERTIES defaultHeapProperties(D3D12_HEAP_TYPE_DEFAULT);
+//
+//		hr = pDevice->CreateCommittedResource(
+//			&defaultHeapProperties,
+//			D3D12_HEAP_FLAG_SHARED,
+//			&desc,
+//			D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE,
+//			nullptr,
+//			IID_PPV_ARGS(&share));
+//
+//	//hr = pDevice->CreateCommittedResource(&heap_props, heap_flags, &res_desc, res_states, clear_value,
+//	//	IID_PPV_ARGS(&share));
+//	if (FAILED(hr)) FatalExit(L"Fail to create shared stereo Texture", hr);
+//
+//	share->SetName(L"Shared double-width stereo texture");
+//
+//	// Now create the HANDLE which is used to share surfaces. 
+//	hr = pDevice->CreateSharedHandle(share, nullptr, GENERIC_ALL, L"stereo_share", &_game_sharedhandle);
+//	if (FAILED(hr) || _game_sharedhandle == NULL) FatalExit(L"Fail to pDevice->CreateSharedHandle", hr);
+//
+//	_shared_texture = share;
+//	pDevice->Release();
+//
+//	LOG(INFO) << "  Successfully created new DX12 _shared_texture: " << _shared_texture << ", new shared _game_sharedhandle: " << _game_sharedhandle;
+//
+//	// Move that shared handle into the MappedView to IPC the Handle to Katanga.
+//	// The HANDLE is always 32 bit, even for 64 bit processes.
+//	// https://docs.microsoft.com/en-us/windows/win32/winprog64/interprocess-communication
+//	//
+//	// This magic line of code will fire off the Katanga side rebuilding of it's drawing pipeline and surface.
+//
+//	*(PUINT)(_mapped_view) = PtrToUint(_game_sharedhandle);
+//
+//	// If we already had created one, let the old one go.  We do it after the recreation
+//	// here fills in the prior globals, to avoid possible dead structure usage in the
+//	// Unity app.
+//
+//	LOG(INFO) << "  Release stale _shared_texture: " << oldGameTexture;
+//	if (oldGameTexture)
+//		oldGameTexture->Release();
 
-	hr = pDevice->CreateCommittedResource(&heap_props, heap_flags, &res_desc, res_states, clear_value,
-		IID_PPV_ARGS(&share));
-	if (FAILED(hr)) FatalExit(L"Fail to create shared stereo Texture", hr);
+	ID3D12Resource* copy;
+	hr = pDevice->OpenSharedHandle(_game_sharedhandle, __uuidof(ID3D12Resource), (LPVOID*)&copy);
+	if (FAILED(hr)) FatalExit(L"Fail to open shared stereo Texture", hr);
+	LOG(INFO) << "  Successfully opened DX12 _game_sharedhandle: " << copy << ", shared _game_sharedhandle: " << _game_sharedhandle;
 
-	share->SetName(L"Shared double-width stereo texture");
 
-	// Now create the HANDLE which is used to share surfaces. 
-	hr = pDevice->CreateSharedHandle(share, nullptr, GENERIC_ALL, L"stereo_share", &_game_sharedhandle);
-	if (FAILED(hr) || _game_sharedhandle == NULL) FatalExit(L"Fail to pDevice->CreateSharedHandle", hr);
-
-	_shared_texture = share;
-	pDevice->Release();
-
-	LOG(INFO) << "  Successfully created new DX12 _shared_texture: " << _shared_texture << ", new shared _game_sharedhandle: " << _game_sharedhandle;
-
-	// Move that shared handle into the MappedView to IPC the Handle to Katanga.
-	// The HANDLE is always 32 bit, even for 64 bit processes.
-	// https://docs.microsoft.com/en-us/windows/win32/winprog64/interprocess-communication
-	//
-	// This magic line of code will fire off the Katanga side rebuilding of it's drawing pipeline and surface.
-
-	*(PUINT)(_mapped_view) = PtrToUint(_game_sharedhandle);
-
-	// If we already had created one, let the old one go.  We do it after the recreation
-	// here fills in the prior globals, to avoid possible dead structure usage in the
-	// Unity app.
-
-	LOG(INFO) << "  Release stale _shared_texture: " << oldGameTexture;
-	if (oldGameTexture)
-		oldGameTexture->Release();
 }
 
 // -----------------------------------------------------------------------------
